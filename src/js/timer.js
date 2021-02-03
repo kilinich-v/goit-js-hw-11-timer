2cc36f682199145a3e553d686a71c297bd4da44e
Vladimir Kilinich <kylynych.v@gmail.com>
2021-02-03 12:34:47 +0200

    добавляет текмплейт таймера

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,31 +1,63 @@
+// прописать старт и стоп
+
 class CountdownTimer {
     constructor({ selector, targetDate }) {
         this.targetDate = targetDate;
+        this.selector = selector;
 
-        this.daysRef = document.querySelector(`${selector} [data-value=days]`);
-        this.hoursRef = document.querySelector(`${selector} [data-value=hours]`);
-        this.minsRef = document.querySelector(`${selector} [data-value=mins]`);
-        this.secsRef = document.querySelector(`${selector} [data-value=secs]`);
+        this.template = `<div class="field" >
+            <span class="value" data-value="days" >00</span>
+                <span class="label">Days</span>
+            </div >
+            <div class="field">
+                <span class="value" data-value="hours">00</span>
+                <span class="label">Hours</span>
+            </div>
+            <div class="field">
+                <span class="value" data-value="mins">00</span>
+                <span class="label">Minutes</span>
+            </div>
+            <div class="field">
+                <span class="value" data-value="secs">00</span>
+                <span class="label">Seconds</span>
+            </div>`;
+        this.root = document.querySelector(this.selector);
+        this.root.insertAdjacentHTML('beforeend', this.template);
+
+        this.daysRef = this.root.querySelector('[data-value=days]');
+        this.hoursRef = this.root.querySelector('[data-value=hours]');
+        this.minsRef = this.root.querySelector('[data-value=mins]');
+        this.secsRef = this.root.querySelector('[data-value=secs]');
+
+        this.intervalId = null;
     }
 
-    updateTimer() {
-        let interval = 1000;
-
+    start() {
+        if (this.intervalId) {
+            return;
+        }
         let deltaTime = Date.now() - this.targetDate;
 
-        setInterval(() => {
-            deltaTime -= 1000
+        this.intervalId = setInterval(() => {
+            if (deltaTime <= 0) {
+                clearInterval(this.intervalId);
+                return;
+            }
 
-            const days = this.pad(Math.floor(deltaTime / (1000 * 60 * 60 * 24)));
-            const hours = this.pad(Math.floor((deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-            const mins = this.pad(Math.floor((deltaTime % (1000 * 60 * 60)) / (1000 * 60)));
-            const secs = this.pad(Math.floor((deltaTime % (1000 * 60)) / 1000));
+            this.updateTimer(deltaTime -= 1000);
+        }, 1000);
+    }
 
-            this.daysRef.textContent = days;
-            this.hoursRef.textContent = hours;
-            this.minsRef.textContent = mins;
-            this.secsRef.textContent = secs;
-        }, interval);
+    updateTimer(time) {
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+        this.daysRef.textContent = days;
+        this.hoursRef.textContent = hours;
+        this.minsRef.textContent = mins;
+        this.secsRef.textContent = secs;
     }
 
     pad(value) {
@@ -33,7 +65,9 @@ class CountdownTimer {
     }
 }
 
-new CountdownTimer({
+const timer = new CountdownTimer({
     selector: '#timer-1',
     targetDate: new Date('Jul 17, 2019'),
-}).updateTimer();
+});
+
+timer.start();
